bb955d8e34a6d7f2e8d828f9b6cf8505576d49b5
rrizun <rrizun@rrizuns-MacBook-Air.local>
2023-05-29 11:18:33 -0700

    __main__

--- ccs.py
+++ ccs.py
@@ -4,20 +4,26 @@ import os
 import requests
 import sys
 
-args = [] # http path args
-params = [] # http query params
 
-knowns, unknowns = argparse.ArgumentParser().parse_known_args()
-for unknown in unknowns:
-    arg = unknown.split('=', 1)
-    if len(arg) == 1:
-        args.append(arg[0])
-    else:
-        params.append((arg[0], arg[1]))
+def main():
+    args = []  # http path args
+    params = []  # http query params
 
-url = '/'.join([os.environ.get('CCS_URL', 'https://api.cirrascale.net'), *args])
-params.append(('_format','text'))
-headers = {'Authorization': os.environ['CCS_KEY']}
-response = requests.post(url, params=params, headers=headers)
-print(response.text)
-sys.exit(0 if response.ok else 1)
+    knowns, unknowns = argparse.ArgumentParser().parse_known_args()
+    for unknown in unknowns:
+        arg = unknown.split('=', 1)
+        if len(arg) == 1:
+            args.append(arg[0])
+        else:
+            params.append((arg[0], arg[1]))
+
+    url = '/'.join([os.environ.get('CCS_URL', 'https://api.cirrascale.net'), *args])
+    params.append(('_format', 'text'))
+    headers = {'Authorization': os.environ['CCS_KEY']}
+    response = requests.post(url, params=params, headers=headers)
+    print(response.text)
+    sys.exit(0 if response.ok else 1)
+
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -1,2 +1,16 @@
 import setuptools
-setuptools.setup(name='ccs-cli', scripts=['ccs.py'])
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'name': 'ccs-cli',
+    'install_requires': ['requests'],
+    'entry_points' : {
+        'console_scripts': ['ccs=ccs:main'],
+    }
+}
+
+setup(**config)

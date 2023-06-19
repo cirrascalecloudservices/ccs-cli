c7b20515a086893dc1d2d89a55542d52f06da13a
rrizun <rrizun@rrizuns-MacBook-Air.local>
2023-06-19 11:34:15 -0700

    CCS_KEY

--- ccs.py
+++ ccs.py
@@ -17,9 +17,13 @@ def main():
         else:
             params.append((arg[0], arg[1]))
 
-    url = '/'.join([os.environ.get('CCS_URL', 'https://api.cirrascale.net'), *args])
+    ccs = os.path.basename(sys.argv[0])
+    api = '-'.join([*reversed(ccs.split('-')[1:]), 'api'])
+    key = '_'.join([*ccs.split('-'), 'key']).upper() # e.g., CCS_KEY
+    # url = '/'.join(['http://localhost:8080', *args])
+    url = '/'.join(['https://{}.cirrascale.net'.format(api), *args])
     params.append(('_format', 'text'))
-    headers = {'Authorization': os.environ['CCS_KEY']}
+    headers = {'Authorization': os.environ[key]}
     response = requests.post(url, params=params, headers=headers)
     print(response.text)
     sys.exit(0 if response.ok else 1)

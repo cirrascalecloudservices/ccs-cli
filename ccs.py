0499cefe39a3a28ca3c02c5168adbfbcd0879c9f
rrizun <rrizun@rrizuns-MacBook-Air.local>
2023-10-05 09:14:04 -0700

    new style

--- ccs.py
+++ ccs.py
@@ -1,31 +1,35 @@
 #!/usr/bin/env python3
 
-import argparse
 import os
 import requests
 import sys
 
-
 def main():
-    args = []  # http path args
+    path = []  # http path
     params = []  # http query params
 
-    knowns, unknowns = argparse.ArgumentParser().parse_known_args()
-    for unknown in unknowns:
-        arg = unknown.split('=', 1)
-        if len(arg) == 1:
-            args.append(arg[0])
+    for arg in sys.argv[1:]:
+        arr = arg.split('=', 1)
+        if len(arr) == 1:
+            if len(path)>0:
+                params.append(('q', arr[0]))
+            else:
+                path.extend([a for a in arr[0].split('/') if a])
         else:
-            params.append((arg[0], arg[1]))
+            params.append((arr[0], arr[1]))
 
-    # url = '/'.join(['http://127.0.0.1:8080', *args])
-    url = '/'.join(['https://api.cirrascale.net', *args])
-    params.append(('_format', 'text'))
+    method = 'get'
+    if len(path)>1:
+        method='post'
+    # url = '/'.join(['http://127.0.0.1:8080', *path])
+    url = '/'.join(['https://api.cirrascale.net', *path])
+    params.append(('_human', '1'))
     headers = {'Authorization': os.environ['CCS_KEY']}
-    response = requests.post(url, params=params, headers=headers)
+    request = requests.Request(method, url, params=params, headers=headers).prepare()
+    print(request.method, request.url, file=sys.stderr)
+    response = requests.Session().send(request)
     print(response.text)
     sys.exit(0 if response.ok else 1)
-
 
 if __name__ == "__main__":
     main()

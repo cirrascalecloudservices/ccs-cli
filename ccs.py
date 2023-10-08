363adb045f631718271bf7b61437e150cdf27456
rrizun <rrizun@rrizuns-MacBook-Air.local>
2023-10-08 07:33:47 -0700

    _human

--- ccs.py
+++ ccs.py
@@ -23,12 +23,13 @@ def main():
         method='post'
     # url = '/'.join(['http://127.0.0.1:8080', *path])
     url = '/'.join(['https://api.cirrascale.net', *path])
-    params.append(('_human', '1'))
     headers = {'Authorization': os.environ['CCS_KEY']}
     request = requests.Request(method, url, params=params, headers=headers).prepare()
     print(request.method, request.url, file=sys.stderr)
+    params.append(('_human', '1'))
+    request = requests.Request(method, url, params=params, headers=headers).prepare()
     response = requests.Session().send(request)
-    print(response.text)
+    print(response.text.strip())
     sys.exit(0 if response.ok else 1)
 
 if __name__ == "__main__":

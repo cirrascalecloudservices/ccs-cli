68b4ef000240d16dd8237096dcadaa7e0a279187
rrizun <(none)>
2024-02-21 08:10:22 -0800

    tabulate

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 config = {
     'name': 'ccs-cli',
     'py_modules': ['ccs'],
-    'install_requires': ['requests'],
+    'install_requires': ['requests', 'tabulate'],
     'entry_points': {
         'console_scripts': ['ccs=ccs:main'],
     }

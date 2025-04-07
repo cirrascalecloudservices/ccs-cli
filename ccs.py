b1075c2077372a131755456e0c38f1a26a38df23
rrizun <(none)>
2025-04-07 05:56:07 -0700

    beginnings of new-style api

--- ccs.py
+++ ccs.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import json
+import time
 
 import requests
 import tabulate
@@ -21,57 +22,56 @@ tablefmt = tabulate.TableFormat(
 )
 
 def ccs():
-    path = []  # http path
-    params = []  # http request params
-    _format = 0
+  action = ''
+  params = [] # http request params
 
-    for arg in sys.argv[1:]:
-        arr = arg.split('=', 1)
-        # named
-        if len(arr) > 1:
-            params.append((arr[0], arr[1]))
-            if arr[0]=='_format':
-                _format=int(arr[1])
-        # unnamed
-        else:
-            if len(path)==0: # first unnamed is noun/verb
-                path.extend(arr[0].split('/'))
-            else:
-                params.append(('q', arr[0]))
-
-    if len(path)<2:
-        if path!=['get']:
-            path.append('get')
-    method = path[-1]
-    if method!='get':
-        method='post'
-    # url = '/'.join(['http://127.0.0.1:8080', *path])
-    url = '/'.join(['https://api.cirrascale.net', *path])
-    ccs_key = os.environ.get('CCS_KEY')
-    if not ccs_key:
-        ccs_key = open('/etc/ccs-key').read().splitlines()[0].split()[0]
-    request = requests.Request(method, url, params=params if method=='get' else None, data=params if method=='post' else None, headers={'Authorization': ccs_key}).prepare()
-    print(request.method, request.url, str(request.body or ''), file=sys.stderr)
-    response = requests.Session().send(request, stream=True)
-
-    if _format:
-        rows=[]
-        for line in response.iter_lines():
-            rows.append(json.loads(line))
-        print(tabulate.tabulate(rows, headers='keys', tablefmt=tablefmt))
+  for arg in sys.argv[1:]:
+    arr = arg.split('=', 1)
+    if len(arr) == 1:
+      if not action:
+        action = arg # e.g., compute/install
+      else:
+        params.append(('q', arg)) # resources, e.g., gpuab12
     else:
-        for line in response.iter_lines():
-            print(line.decode())
+      params.append((arr[0], arr[1])) # params, e.g., image=ubuntu/focal
 
+  url = os.environ.get('CCS_URL') or 'https://api.cirrascale.net'
+  ccs_key = os.environ.get('CCS_KEY') or open('/etc/ccs-key').read().splitlines()[0].split()[0]
+
+  if ':' in action: # new style, e.g., always json, paging, work-queue, etc..
+    method = 'get' if action.split(':')[1] == 'get' else 'post'
+    next = requests.Request(method, f'{url}/{action}', params=params).prepare().url
+    while next:
+      request = requests.Request(method, next, headers={'Authorization': ccs_key}).prepare()
+      print(request.method, request.url, file=sys.stderr) # debug
+      response = requests.Session().send(request)
+      if response.ok:
+        next = response.json().get('next')
+        for line in response.json()['value']:
+          print(json.dumps(line), flush=True)
+        time.sleep(int(response.headers.get('retry-after', '0'))) # honor retry-after seconds
+      else:
+        print(json.dumps(response.json()))
+      method = 'get'
+      response.raise_for_status()
+  else: # old style
+    if not action:
+      action='get'
+    if action!='get' and len(action.split('/'))<2:
+      action = '/'.join([*action.split('/'), 'get'])
+    method = 'get' if action.split('/')[-1] == 'get' else 'post'
+    request = requests.Request(method, f'{url}/{action}', params=params if method=='get' else None, data=params if method=='post' else None, headers={'Authorization': ccs_key}).prepare()
+    print(request.method, request.url, str(request.body or ''), file=sys.stderr) # debug
+    response = requests.Session().send(request, stream=True)
+    for line in response.iter_lines():
+      print(line.decode(), flush=True)
     sys.exit(0 if response.ok else 1)
 
-
 def ccsfmt():
-    rows = []
-    for line in sys.stdin:
-        rows.append(json.loads(line))
-    print(tabulate.tabulate(rows, headers="keys", tablefmt=tablefmt))
-
+  rows = []
+  for line in sys.stdin:
+    rows.append(json.loads(line))
+  print(tabulate.tabulate(rows, headers='keys', tablefmt=tablefmt))
 
 if __name__ == "__main__":
-    ccs()
+  ccs()
